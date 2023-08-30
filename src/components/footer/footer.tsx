50a53f03cd9efc0638b01dbd86a7124e8d8b418d
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-08-30 10:37:14 +0300

    * footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,38 +1,26 @@
-import { SocialLink } from '@/components/social-link/social-link';
+import Link from 'next/link';
+
+import { Icon } from '@/components/icon/icon';
 import styles from './styles.module.scss';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <h2 className={styles.title}>Contacts:</h2>
-
-        <div className={styles.social}>
-          <SocialLink
-            className={styles.socialLink}
-            href="mailto:nailheart4@gmail.com"
-            iconName="email"
-            title="email"
-          />
-          <SocialLink
-            className={styles.socialLink}
-            href="linkedin.com/in/yaroslav-lebedenko-9b1bb7233"
-            iconName="linkedin"
-            title="linkedin"
-          />
-          <SocialLink
-            className={styles.socialLink}
-            href="https://github.com/Nailheart"
-            iconName="github"
-            title="github"
-          />
-          <SocialLink
-            className={styles.socialLink}
-            href="https://join.skype.com/invite/D5WcE8DUXPR9"
-            iconName="skype"
-            title="skype"
-          />
-        </div>
+        <p className={styles.copyright}>
+          <span>
+            © {new Date().getFullYear().toString()}&nbsp;
+            Made with <span className={styles.heart}>&#10084;</span> by&nbsp;
+          </span>
+          <Link className={styles.name} href="/">
+            Yaroslav&nbsp;Lebedenko
+            <Icon
+              className={styles.icon}
+              name="rocket"
+              aria-label="rocket icon"
+            />
+          </Link>
+        </p>
       </div>
     </footer>
   );
