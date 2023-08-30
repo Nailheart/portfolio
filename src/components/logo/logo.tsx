db27b02c326a03d2c2f49be34c91da82c25ad6f3
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-08-30 10:37:53 +0300

    * logo component

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 import { cn } from '@/helpers/helpers';
 import styles from './styles.module.scss';
@@ -12,14 +11,7 @@ type Props = {
 const Logo: FC<Props> = ({ className }) => {
   return (
     <Link className={cn(styles.logo, className)} href="/">
-      <Image
-        className={styles.img}
-        src="/img/logo.svg"
-        width="60"
-        height="35"
-        alt="Logo"
-      />
-      <span className={styles.text}>Nailheart</span>
+      <span>YLebedenko.</span>
     </Link>
   );
 };
