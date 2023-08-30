ecff438f32db0af662f6d41ea3e8886a3f9734c6
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-08-30 10:43:46 +0300

    + home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
+import { Intro } from '@/components/intro/intro';
+import { About } from '@/components/about/about';
+import { Portfolio } from '@/components/portfolio/portfolio';
+import { Contacts } from '@/components/contacts/contacts';
 import styles from './styles.module.scss';
 
 const Home = () => {
   return (
-    <section className={styles.home}>
-      <div className={styles.container}>
-        <div className={styles.descripiton}>
-          <h1 className={styles.title}>
-            Hello there! 👋<br/>
-            I'm <span>Yaroslav Lebedenko</span>
-          </h1>
-          <p className={styles.text}>I'm a front-end developer from Ukraine, and this is my small portfolio website. Thank you for visiting, and I hope you enjoy exploring my portfolio.</p>
-        </div>
-
-        <div className={styles.photo}></div>
-      </div>
-    </section>
+    <div className={styles.container}>
+      <Intro />
+      <About />
+      <Portfolio />
+      <Contacts />
+    </div>
   );
 };
 
