0939abcc5605fd039aadc616b41fe5e25c9b5b88
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-10-23 18:46:15 +0300

    remove comments

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -4,16 +4,20 @@ import styles from './styles.module.scss';
 const Intro = () => {
   return (
     <section id="home" className={styles.intro}>
-      {/* TODO: add coffee animation or enable / disable monitor */}
       <PC className={styles.pc} />
 
       <div className={styles.description}>
         <h1 className={styles.title}>
-          Hello there! 👋<br/>
+          Hello there! 👋
+          <br />
           I&apos;m <span>Yaroslav Lebedenko</span>
         </h1>
 
-        <p className={styles.text}>I&apos;m a frontend developer from Ukraine, and this is my small portfolio website. Thank you for visiting, and I hope you enjoy exploring my portfolio.</p>
+        <p className={styles.text}>
+          I&apos;m a frontend developer from Ukraine, and this is my small
+          portfolio website. Thank you for visiting, and I hope you enjoy
+          exploring my portfolio.
+        </p>
       </div>
     </section>
   );
